d4cbac60acf57e143a163e95169ae8875fae2184
Mathilde <ceccaroli.m@gmail.com>
2018-02-13 17:56:58 +1000

    [#4] program startup

--- LiftTravelControl.Tests/ProgramTests.cs
+++ LiftTravelControl.Tests/ProgramTests.cs
@@ -6,9 +6,27 @@ namespace LiftTravelControl.Tests
     public class ProgramTests
     {
         [Fact]
-        public void Program_MustThrowExecption_WhenCurrentParkedFloorValueNotRecievedAtStart()
+        public void Program_MustThrowExecption_WhenNoParameterPassedAtStart()
         {
             Assert.Throws<ArgumentException>(() => Program.Main(null));
+        }
+
+        [Theory]
+        [InlineData("Foo")]
+        [InlineData("7.5")]
+        [InlineData("")]
+        public void Program_MustThrowExecption_WhenCurrentParkedFloorValueParameterNotInteger(string testValue)
+        {
+            Assert.Throws<ArgumentException>(() => Program.Main(new[] { testValue }));
+        }
+
+        [Theory]
+        [InlineData("-2")]
+        [InlineData("11")]
+        [InlineData("145")]
+        public void Program_MustThrowExecption_WhenCurrentParkedFloorValueParameterNotIn1To10Range(string testValue)
+        {
+            Assert.Throws<ArgumentException>(() => Program.Main(new[] { testValue }));
         }
     }
 }
--- LiftTravelControl/Program.cs
+++ LiftTravelControl/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Linq;
 
 namespace LiftTravelControl
 {
     internal class Program
     {
+        private const int liftMinFloor = 1;
+        private const int liftMaxFloor = 10;
+
+        private const int liftMinFloor0Based = liftMinFloor - 1;
+        private const int liftMaxFloor0Based = liftMaxFloor - 1;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args == null
+                || !args.Any())
+            {
+                throw new ArgumentException("Missing lift floor parameter");
+            }
+
+            int currentParkedFloorValue = GetCurrentFloorValue(args);
+
+            if (!IsValidFloor(currentParkedFloorValue))
+            {
+                throw new ArgumentException($"Unknown floor value: {currentParkedFloorValue}");
+            }
+        }
+
+        private static bool IsValidFloor(int currentParkedFloorValue)
+        {
+            return currentParkedFloorValue >= liftMinFloor && currentParkedFloorValue <= liftMaxFloor;  
+        }
+
+        private static int GetCurrentFloorValue(string[] args)
+        {
+            int currentParkedFloorValue;
+            bool isNumberFloorValue = int.TryParse(args.First(), out currentParkedFloorValue);
+
+            if (!isNumberFloorValue)
+            {
+                throw new ArgumentException($"Invalid floor value: {args.First()}");
+            }
+
+            return currentParkedFloorValue;
         }
     }
 }
